25dce4eebb2399829d9c111a7616a4ee2571006d
edmundwsy <edmundwsy@outlook.com>
2023-02-26 20:23:30 +0100

    sync multi_env_kitty script

--- script/multi_env_kitty.py
+++ script/multi_env_kitty.py
@@ -24,7 +24,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--world", type=str, default="_mader_4uav_19obs", help="world name")
 parser.add_argument("--iters", type=int, default="1", help="number of agents")
 parser.add_argument("--num_agents", type=int, default="4", help="number of agents")
-parser.add_argument("--save_path", type=str, default="results", help="folder name")
+parser.add_argument("--save_path", type=str, default="cylinder", help="folder name")
 parser.add_argument("--waiting_time", type=float, default="20", help="waiting time")
 parser.add_argument(
     "--node_name",
@@ -56,11 +56,11 @@ parser.add_argument(
 if __name__ == "__main__":
     args = parser.parse_args()
     launch_cmds = [
-        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=false obs_num:=10 map_mode:=0",
-        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=false obs_num:=20 map_mode:=0",
-        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=false obs_num:=30 map_mode:=0",
-        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=false obs_num:=40 map_mode:=0",
-        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=false obs_num:=50 map_mode:=0",
+        # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=10 map_mode:=0",
+        # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=false obs_num:=20 map_mode:=0",
+        # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=false obs_num:=30 map_mode:=0",
+        # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=false obs_num:=40 map_mode:=0",
+        # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=false obs_num:=50 map_mode:=0",
         # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true map_mode:=1",
         # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true map_mode:=2",
         # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=10 map_mode:=1",
@@ -71,23 +71,63 @@ if __name__ == "__main__":
         # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=40 map_mode:=2",
         # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=50 map_mode:=1",
         # "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=50 map_mode:=2",
-        # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=false obs_num:=10",
+        # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=10",
         # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=false obs_num:=20",
         # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=false obs_num:=30",
         # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=false obs_num:=40",
         # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=false obs_num:=50",
-        # "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=false obs_num:=10",
-        # "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=false obs_num:=20",
-        # "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=false obs_num:=30",
-        # "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=false obs_num:=40",
+        # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=5",
+        # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=15",
+        # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=25",
+        # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=35",
+        # "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=45",
+        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=50",
+        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=40",
+        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=30",
+        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=20",
+        "roslaunch mader sim_fkpcp_4_case_0.launch rviz:=true obs_num:=10",
+        "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=50",
+        "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=40",
+        "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=30",
+        "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=20",
+        "roslaunch mader sim_fkpcp_4_case_3.launch rviz:=true obs_num:=10",
+        "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=true obs_num:=50",
+        "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=true obs_num:=40",
+        "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=true obs_num:=30",
+        "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=true obs_num:=20",
+        "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=true obs_num:=10",
         # "roslaunch mader sim_fkpcp_4_case_4.launch rviz:=false obs_num:=50",
     ]
+    trigger_cmds = [
+        # "rosrun mader sim_goal_publisher.py case_0",
+        # "roslaunch mader multi_goal.launch action:=case_0",
+        # "roslaunch mader multi_goal.launch action:=case_0",
+        # "roslaunch mader multi_goal.launch action:=case_0",
+        # "roslaunch mader multi_goal.launch action:=case_0",
+        # "roslaunch mader multi_goal.launch action:=case_3",
+        "roslaunch mader multi_goal.launch action:=case_0",
+        "roslaunch mader multi_goal.launch action:=case_0",
+        "roslaunch mader multi_goal.launch action:=case_0",
+        "roslaunch mader multi_goal.launch action:=case_0",
+        "roslaunch mader multi_goal.launch action:=case_0",
+        "roslaunch mader multi_goal.launch action:=case_3",
+        "roslaunch mader multi_goal.launch action:=case_3",
+        "roslaunch mader multi_goal.launch action:=case_3",
+        "roslaunch mader multi_goal.launch action:=case_3",
+        "roslaunch mader multi_goal.launch action:=case_3",
+        "roslaunch mader multi_goal.launch action:=case_4",
+        "roslaunch mader multi_goal.launch action:=case_4",
+        "roslaunch mader multi_goal.launch action:=case_4",
+        "roslaunch mader multi_goal.launch action:=case_4",
+        "roslaunch mader multi_goal.launch action:=case_4",
+    ]
+
     world_names = [
-        "mader_4uav_case0_10obs_v2_a6_cls0.15_ir1.5",
-        "mader_4uav_case0_20obs_v2_a6_cls0.15_ir1.5",
-        "mader_4uav_case0_30obs_v2_a6_cls0.15_ir1.5",
-        "mader_4uav_case0_40obs_v2_a6_cls0.15_ir1.5",
-        "mader_4uav_case0_50obs_v2_a6_cls0.15_ir1.5",
+        # "mader_4uav_case0_10obs_v2_a6_cls0.15_ir1.5",
+        # "mader_4uav_case0_20obs_v2_a6_cls0.15_ir1.5",
+        # "mader_4uav_case0_30obs_v2_a6_cls0.15_ir1.5",
+        # "mader_4uav_case0_40obs_v2_a6_cls0.15_ir1.5",
+        # "mader_4uav_case0_50obs_v2_a6_cls0.15_ir1.5",
         # "mader_4uav_case1_20obs_v2_a6_cls0.15_ir1.5",
         # "mader_4uav_case2_20obs_v2_a6_cls0.15_ir1.5",
         # "mader_4uav_case1_10obs_v2_a6_cls0.15_ir1.5",
@@ -103,18 +143,29 @@ if __name__ == "__main__":
         # "mader_4uav_case3_30obs_v2_a6_cls0.15",
         # "mader_4uav_case3_40obs_v2_a6_cls0.15",
         # "mader_4uav_case3_50obs_v2_a6_cls0.15",
-        # "mader_4uav_case4_10obs_v2_a6_cls0.15",
-        # "mader_4uav_case4_20obs_v2_a6_cls0.15",
-        # "mader_4uav_case4_30obs_v2_a6_cls0.15",
-        # "mader_4uav_case4_40obs_v2_a6_cls0.15",
-        # "mader_4uav_case4_50obs_v2_a6_cls0.15",
+        "mader_4uav_case0_50obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case0_40obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case0_30obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case0_20obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case0_10obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case3_50obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case3_40obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case3_30obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case3_20obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case3_10obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case4_50obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case4_40obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case4_30obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case4_20obs_v2_a6_cls0.15_ir1.5",
+        "mader_4uav_case4_10obs_v2_a6_cls0.15_ir1.5",
     ]
     for i in range(args.iters):
         print("=====    Iteration: {}    =====".format(i))
-        for (cmd, world_name) in zip(launch_cmds, world_names):
+        for (cmd, trigger, world_name) in zip(launch_cmds, trigger_cmds, world_names):
             print("=====    World: {}    =====".format(world_name))
             args.cmd_launch = cmd
             args.world = world_name
+            args.cmd_trigger = trigger
             run(args)
             time.sleep(1)
 
