8f0d2aad2bf42111e749c7848dbb8ab3454a9a37
edmundwsy <edmundwsy@outlook.com>
2023-08-08 00:18:08 +0200

    add noise in communication

--- script/multi_env_kitty.py
+++ script/multi_env_kitty.py
@@ -18,7 +18,9 @@ parser.add_argument(
 )
 parser.add_argument("--iters", type=int, default="1", help="number of agents")
 parser.add_argument("--num_agents", type=int, default="4", help="number of agents")
-parser.add_argument("--save_path", type=str, default="results", help="folder name")
+parser.add_argument(
+    "--save_path", type=str, default="resultsnormal", help="folder name"
+)
 parser.add_argument("--waiting_time", type=float, default="20", help="waiting time")
 parser.add_argument(
     "--node_name", type=str, default="/uav0/planner", help="key planning node name"
@@ -56,6 +58,12 @@ if __name__ == "__main__":
         # "roslaunch plan_manager sim_fkpcp_4_case_1.launch rviz:=false obs_num:=30",
         # "roslaunch plan_manager sim_fkpcp_4_case_1.launch rviz:=false obs_num:=20",
         # "roslaunch plan_manager sim_fkpcp_4_case_1.launch rviz:=false obs_num:=10",
+        # "roslaunch plan_manager sim_fkpcp_4_case_3.launch rviz:=false obs_num:=50",
+        # "roslaunch plan_manager sim_fkpcp_4_case_3.launch rviz:=false obs_num:=40",
+        # "roslaunch plan_manager sim_fkpcp_4_case_3.launch rviz:=false obs_num:=30",
+        # "roslaunch plan_manager sim_fkpcp_4_case_3.launch rviz:=false obs_num:=20",
+        # "roslaunch plan_manager sim_fkpcp_4_case_3.launch rviz:=false obs_num:=10",
+        # "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=50",
         "roslaunch plan_manager sim_fkpcp_4_case_0.launch rviz:=false obs_num:=50",
         "roslaunch plan_manager sim_fkpcp_4_case_0.launch rviz:=false obs_num:=40",
         "roslaunch plan_manager sim_fkpcp_4_case_0.launch rviz:=false obs_num:=30",
@@ -66,28 +74,24 @@ if __name__ == "__main__":
         "roslaunch plan_manager sim_fkpcp_4_case_3.launch rviz:=false obs_num:=30",
         "roslaunch plan_manager sim_fkpcp_4_case_3.launch rviz:=false obs_num:=20",
         "roslaunch plan_manager sim_fkpcp_4_case_3.launch rviz:=false obs_num:=10",
-        "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=10",
-        "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=20",
-        "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=30",
-        "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=40",
         "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=50",
+        "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=40",
+        "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=30",
+        "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=20",
+        "roslaunch plan_manager sim_fkpcp_4_case_4.launch rviz:=false obs_num:=10",
     ]
     world_names = [
-        "ours_4uav_case0_50obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case0_40obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case0_30obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case0_20obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case0_10obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case3_50obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case3_40obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case3_30obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case3_20obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case3_10obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case4_10obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case4_20obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case4_30obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case4_40obs_v2_a6_cls0.15_ir1.5",
-        "ours_4uav_case4_50obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case0_50obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case0_40obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case0_30obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case0_20obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case0_10obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case3_50obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case3_40obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case3_30obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case3_20obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case3_10obs_v2_a6_cls0.15_ir1.5",
+        # "ours_4uav_case4_50obs_v2_a6_cls0.15_ir1.5",
         # "ours_4uav_case2_50obs_v2_a6_cls0.15_ir1.5",
         # "ours_4uav_case2_40obs_v2_a6_cls0.15_ir1.5",
         # "ours_4uav_case2_30obs_v2_a6_cls0.15_ir1.5",
@@ -98,6 +102,21 @@ if __name__ == "__main__":
         # "ours_4uav_case1_30obs_v2_a6_cls0.15_ir1.5",
         # "ours_4uav_case1_20obs_v2_a6_cls0.15_ir1.5",
         # "ours_4uav_case1_10obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case0_50obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case0_40obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case0_30obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case0_20obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case0_10obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case3_50obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case3_40obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case3_30obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case3_20obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case3_10obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case4_50obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case4_40obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case4_30obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case4_20obs_v2_a6_cls0.15_ir1.5",
+        "ours_4uav_case4_10obs_v2_a6_cls0.15_ir1.5",
     ]
     for i in range(args.iters):
         print("=====    Iteration: {}    =====".format(i))
