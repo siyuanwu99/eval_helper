f8a2ff186b1d2f5c4e41d1a5bc1de502e280622e
edmundwsy <edmundwsy@outlook.com>
2023-08-08 17:02:07 +0200

    changes on ego

--- script/test2_multi_env_kitty.py
+++ script/test2_multi_env_kitty.py
@@ -17,22 +17,23 @@ parser.add_argument(
     "--world", type=str, default="_4uav_20obs_v2_a8_cls0.15_", help="world name"
 )
 parser.add_argument("--iters", type=int, default="1", help="number of agents")
-parser.add_argument("--num_agents", type=int,
-                    default="8", help="number of agents")
+parser.add_argument("--num_agents", type=int, default="8", help="number of agents")
 parser.add_argument(
-    "--save_path", type=str, default="resultsnormal", help="folder name"
+    "--save_path", type=str, default="resultsnoisy_", help="folder name"
 )
-parser.add_argument("--waiting_time", type=float,
-                    default="20", help="waiting time")
+parser.add_argument("--waiting_time", type=float, default="20", help="waiting time")
 parser.add_argument(
-    "--node_name", type=str, default="/uav0/planner", help="key planning node name"
+    "--node_name",
+    type=str,
+    default="/drone_0_ego_planner_node",
+    help="key planning node name",
 )
 parser.add_argument("--no-run", action="store_true")
 
 parser.add_argument(
     "--cmd_source",
     type=str,
-    default="source /home/siyuan/workspace/thesis_workspace/devel/setup.bash",
+    default="source /home/siyuan/workspace/dyn_env_ego-swarm/devel/setup.bash",
 )
 parser.add_argument(
     "--cmd_recorder",
@@ -42,61 +43,60 @@ parser.add_argument(
 parser.add_argument(
     "--cmd_launch",
     type=str,
-    default="roslaunch plan_manager sim_baseline_fkpcp_4.launch rviz:=false",
-    # default="roslaunch plan_manager sim_baseline_fkpcp_4.launch rviz:=true",
+    default="roslaunch ego_planner dyn_evaluate.launch rviz:=false",
+    # default="roslaunch ego_planner dyn_evaluate.launch rviz:=true",
 )
-parser.add_argument("--cmd_trigger", type=str,
-                    default="rosrun eval_helper trigger")
+parser.add_argument("--cmd_trigger", type=str, default="rosrun eval_helper trigger")
 
 if __name__ == "__main__":
     args = parser.parse_args()
     launch_cmds = [
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.01 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.02 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.03 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.04 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.05 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.1",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.2",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.3",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.4",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.5",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.01 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.02 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.03 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.04 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.05 tracking_stddev:=0.0",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.1",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.2",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.3",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.4",
-        "roslaunch plan_manager sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.5",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.01 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.02 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.03 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.04 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.05 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.1",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.2",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.3",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.4",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=00 time_sync_stddev:=0.00 tracking_stddev:=0.5",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.01 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.02 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.03 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.04 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.05 tracking_stddev:=0.0",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.1",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.2",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.3",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.4",
+        "roslaunch ego_planner sim_fkpcp_8_case_1.launch rviz:=false obs_num:=20 time_sync_stddev:=0.00 tracking_stddev:=0.5",
     ]
     world_names = [
-        "oursnoisy_8uav_00obs_v2_a6_ts0.00_tr0.0_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.01_tr0.0_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.02_tr0.0_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.03_tr0.0_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.04_tr0.0_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.05_tr0.0_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.00_tr0.1_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.00_tr0.2_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.00_tr0.3_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.00_tr0.4_",
-        "oursnoisy_8uav_00obs_v2_a6_ts0.00_tr0.5_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.00_tr0.0_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.01_tr0.0_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.02_tr0.0_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.03_tr0.0_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.04_tr0.0_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.05_tr0.0_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.00_tr0.1_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.00_tr0.2_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.00_tr0.3_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.00_tr0.4_",
-        "oursnoisy_8uav_20obs_v2_a6_ts0.00_tr0.5_",
+        "ego_8uav_00obs_v2_a6_ts0.00_tr0.0_",
+        "ego_8uav_00obs_v2_a6_ts0.01_tr0.0_",
+        "ego_8uav_00obs_v2_a6_ts0.02_tr0.0_",
+        "ego_8uav_00obs_v2_a6_ts0.03_tr0.0_",
+        "ego_8uav_00obs_v2_a6_ts0.04_tr0.0_",
+        "ego_8uav_00obs_v2_a6_ts0.05_tr0.0_",
+        "ego_8uav_00obs_v2_a6_ts0.00_tr0.1_",
+        "ego_8uav_00obs_v2_a6_ts0.00_tr0.2_",
+        "ego_8uav_00obs_v2_a6_ts0.00_tr0.3_",
+        "ego_8uav_00obs_v2_a6_ts0.00_tr0.4_",
+        "ego_8uav_00obs_v2_a6_ts0.00_tr0.5_",
+        "ego_8uav_20obs_v2_a6_ts0.00_tr0.0_",
+        "ego_8uav_20obs_v2_a6_ts0.01_tr0.0_",
+        "ego_8uav_20obs_v2_a6_ts0.02_tr0.0_",
+        "ego_8uav_20obs_v2_a6_ts0.03_tr0.0_",
+        "ego_8uav_20obs_v2_a6_ts0.04_tr0.0_",
+        "ego_8uav_20obs_v2_a6_ts0.05_tr0.0_",
+        "ego_8uav_20obs_v2_a6_ts0.00_tr0.1_",
+        "ego_8uav_20obs_v2_a6_ts0.00_tr0.2_",
+        "ego_8uav_20obs_v2_a6_ts0.00_tr0.3_",
+        "ego_8uav_20obs_v2_a6_ts0.00_tr0.4_",
+        "ego_8uav_20obs_v2_a6_ts0.00_tr0.5_",
     ]
     for i in range(args.iters):
         print("=====    Iteration: {}    =====".format(i))
--- src/traj_noise_maker.cpp
+++ src/traj_noise_maker.cpp
@@ -12,7 +12,7 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <ros/ros.h>
-#include <traj_utils/BezierTraj.h>
+#include <traj_utils/MINCOTraj.h>
 #include <iostream>
 #include <random>
 
@@ -33,10 +33,9 @@ std::uniform_real_distribution<double> tracking_noise_;
 int                 num_agents_;
 std::vector<double> sync_errors_;
 
-void trajCallback(const traj_utils::BezierTrajConstPtr& msg) {
+void trajCallback(const traj_utils::MINCOTrajConstPtr& msg) {
   int       id      = msg->drone_id;
   ros::Time t_start = msg->start_time;
-  ros::Time t_pub   = msg->pub_time;
 
   if (id >= num_agents_) {
     ROS_ERROR_STREAM("Agent id " << id << " is out of range of " << num_agents_ << " agents.");
@@ -48,27 +47,50 @@ void trajCallback(const traj_utils::BezierTrajConstPtr& msg) {
       t_start + ros::Duration(sync_errors_[id]) + ros::Duration(tracking_noise_(ng_));
   /* @NOTE: the tracking error is considered as the difference between the
    * start time of the trajectory */
-  ros::Time noisy_t_pub = t_pub + ros::Duration(sync_errors_[id]);
+  traj_utils::MINCOTraj noisy_traj;
+  noisy_traj.drone_id      = msg->drone_id;
+  noisy_traj.traj_id       = msg->traj_id;
+  noisy_traj.order         = msg->order;
+  noisy_traj.start_time    = noisy_t_start;
+  noisy_traj.des_clearance = msg->des_clearance;
+  noisy_traj.shape[0]      = msg->shape[0];
+  noisy_traj.shape[1]      = msg->shape[1];
+  noisy_traj.shape[2]      = msg->shape[2];
 
-  traj_utils::BezierTraj noisy_traj;
-  noisy_traj.drone_id   = msg->drone_id;
-  noisy_traj.traj_id    = msg->traj_id;
-  noisy_traj.order      = msg->order;
-  noisy_traj.duration   = msg->duration;
-  noisy_traj.start_time = noisy_t_start;
-  noisy_traj.pub_time   = noisy_t_pub;
+  noisy_traj.start_p[0] = msg->start_p[0] + loc_noise_x_(ng_);
+  noisy_traj.start_p[1] = msg->start_p[1] + loc_noise_y_(ng_);
+  noisy_traj.start_p[2] = msg->start_p[2] + loc_noise_z_(ng_);
 
+  noisy_traj.start_v[0] = msg->start_v[0];
+  noisy_traj.start_v[1] = msg->start_v[1];
+  noisy_traj.start_v[2] = msg->start_v[2];
+  noisy_traj.start_a[0] = msg->start_a[0];
+  noisy_traj.start_a[1] = msg->start_a[1];
+  noisy_traj.start_a[2] = msg->start_a[2];
+
+  noisy_traj.end_p[0] = msg->end_p[0] + loc_noise_x_(ng_);
+  noisy_traj.end_p[1] = msg->end_p[1] + loc_noise_y_(ng_);
+  noisy_traj.end_p[2] = msg->end_p[2] + loc_noise_z_(ng_);
+  noisy_traj.end_v[0] = msg->end_v[0];
+  noisy_traj.end_v[1] = msg->end_v[1];
+  noisy_traj.end_v[2] = msg->end_v[2];
+  noisy_traj.end_a[0] = msg->end_a[0];
+  noisy_traj.end_a[1] = msg->end_a[1];
+  noisy_traj.end_a[2] = msg->end_a[2];
+
+  noisy_traj.inner_x.resize(msg->inner_x.size());
+  noisy_traj.inner_y.resize(msg->inner_y.size());
+  noisy_traj.inner_z.resize(msg->inner_z.size());
+  for (int i = 0; i < msg->inner_x.size(); ++i) {
+    noisy_traj.inner_x[i] = msg->inner_x[i] + loc_noise_x_(ng_);
+    noisy_traj.inner_y[i] = msg->inner_y[i] + loc_noise_y_(ng_);
+    noisy_traj.inner_z[i] = msg->inner_z[i] + loc_noise_z_(ng_);
+  }
   noisy_traj.duration.resize(msg->duration.size());
-  for (int i = 0; i < msg->duration.size(); ++i) {
+  for (int i = 0; i < msg->duration.size() + 1; ++i) {
     noisy_traj.duration[i] = msg->duration[i];
   }
-  int n_pts = msg->cpts.size();
-  noisy_traj.cpts.resize(n_pts);
-  for (int i = 0; i < n_pts; ++i) {
-    noisy_traj.cpts[i].x = msg->cpts[i].x + loc_noise_x_(ng_);
-    noisy_traj.cpts[i].y = msg->cpts[i].y + loc_noise_y_(ng_);
-    noisy_traj.cpts[i].z = msg->cpts[i].z + loc_noise_z_(ng_);
-  }
+
   noisy_traj_pub_.publish(noisy_traj);
 }
 
@@ -107,9 +129,9 @@ int main(int argc, char** argv) {
     sync_errors_[i] = sync_noise_(ng_);
   }
 
-  ros::Subscriber traj_sub =
-      nh.subscribe("/broadcast_traj", 1, &trajCallback, ros::TransportHints().tcpNoDelay());
-  noisy_traj_pub_ = nh.advertise<traj_utils::BezierTraj>("/noisy_traj", 1);
+  ros::Subscriber traj_sub = nh.subscribe("/broadcast_traj_to_planner", 1, &trajCallback,
+                                          ros::TransportHints().tcpNoDelay());
+  noisy_traj_pub_ = nh.advertise<traj_utils::MINCOTraj>("/noisy_broadcast_traj_to_planner", 1);
 
   ros::spin();
 }
